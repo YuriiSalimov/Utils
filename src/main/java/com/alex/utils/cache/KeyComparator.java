a9e33d283479460854fa16ba01a840ec8ce319b7
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-04-29 12:51:32 +0300

    Initial commit

---
+++ src/main/java/com/alex/utils/cache/KeyComparator.java
@@ -0,0 +1,34 @@
+package com.alex.utils.cache;
+
+import java.util.Comparator;
+
+import static java.util.Objects.isNull;
+
+/**
+ * The class implements a set of methods for working
+ * with comparators for {@link Key}.
+ *
+ * @author Yurii Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ */
+final class KeyComparator implements Comparator<Key> {
+
+    /**
+     * The class implements a method for working with comparator
+     * for {@link Key} by timeout.
+     */
+    @Override
+    public int compare(Key key1, Key key2) {
+        int result;
+        if (isNull(key1) && isNull(key2)) {
+            result = 0;
+        } else if (isNull(key1)) {
+            result = -1;
+        } else if (isNull(key2)) {
+            result = 1;
+        } else {
+            result = (int) (key1.getTimeout() - key2.getTimeout());
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/alex/utils/compressor/HtmlCompressor.java
@@ -0,0 +1,30 @@
+package com.alex.utils.compressor;
+
+import com.googlecode.htmlcompressor.compressor.Compressor;
+
+/**
+ * The class implements a set of methods for compressing HTML data.
+ *
+ * @author Yuriy Salimov (yuriy.alex.salimov@gmail.com)
+ * @version 1.0
+ */
+public final class HtmlCompressor extends AbstractCompressor implements Compressor {
+
+    /**
+     * Remove Intertag Spaces.
+     */
+    private final static boolean REMOVED_INTERTAG_SPACES = true;
+
+    /**
+     * Returns Compressor object.
+     *
+     * @return The HTML compressor instance.
+     */
+    @Override
+    protected Compressor getCompressor() {
+        final com.googlecode.htmlcompressor.compressor.HtmlCompressor compressor =
+                new com.googlecode.htmlcompressor.compressor.HtmlCompressor();
+        compressor.setRemoveIntertagSpaces(REMOVED_INTERTAG_SPACES);
+        return compressor;
+    }
+}
